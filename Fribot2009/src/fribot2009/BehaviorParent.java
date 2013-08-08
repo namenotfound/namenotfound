ab557b0d85c809bf00cd2c72001940ad2a6b964c
namenotfound <_>
2013-08-08 10:26:16 +0200

    FUUUUU

--- Fribot2009/src/fribot2009/BehaviorParent.java
+++ Fribot2009/src/fribot2009/BehaviorParent.java
@@ -1,5 +1,34 @@
 package fribot2009;
 
-public class BehaviorParent {
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.subsumption.Behavior;
+
+public class BehaviorParent implements Behavior {
+	
+	protected boolean suppressed = false;
+	protected boolean executed = false;
+	protected DifferentialPilot pilot;
+	protected Navigator navi;
+	
+	public BehaviorParent(DifferentialPilot pilot, Navigator navi){
+		this.pilot = pilot;
+		this.navi = navi;
+	}
+
+	@Override
+	public boolean takeControl() {
+		return executed;
+	}
+
+	@Override
+	public void action() {
+		suppressed = false;
+	}
+
+	@Override
+	public void suppress() {
+		suppressed = true;
+	}
 
 }

76a6f6a8c403512ffdfc11df8d8d14f538c92413
Cyrill <Cyrill@ReAlSwIsS>
2013-08-08 11:03:30 +0200

    .

--- Fribot2009/src/fribot2009/Fribot2009.java
+++ Fribot2009/src/fribot2009/Fribot2009.java
@@ -1,7 +1,11 @@
 package fribot2009;
 
 import lejos.nxt.Button;
+import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.Navigator;
 import lejos.robotics.navigation.Pose;
@@ -14,12 +18,16 @@ public class Fribot2009 {
 	private Arbitrator arbi;
 	private DifferentialPilot pilot;
 	private Navigator navi;
+	private UltrasonicSensor us;
+	private ColorSensor cs;
+	private TouchSensor ts1;
+	private TouchSensor ts2;
 	
 	public Fribot2009()
 	{
 		initPilot();
 		initNavi();
-		
+		initSensors();
 		Behavior[] bev={new GotoFinish(pilot, navi),new PlaceStilo(pilot, navi),new PlaceStilo(pilot, navi),new SearchSlot(pilot, navi)
 	//	,new Calibrate(pilot, navi)
 		,new Abort(pilot, navi)
@@ -37,7 +45,13 @@ public class Fribot2009 {
 		navi=new Navigator(pilot);
 		navi.getPoseProvider().setPose(new Pose(0, 0, 0));
 	}
-	
+	private void initSensors()
+	{
+		us=new UltrasonicSensor(SensorPort.S4);
+		cs=new ColorSensor(SensorPort.S3);
+		ts1=new TouchSensor(SensorPort.S1);
+		ts2=new TouchSensor(SensorPort.S2);
+	}
 	
 	
 	public void run()
--- Fribot2009/src/fribot2009/PlaceStilo.java
+++ Fribot2009/src/fribot2009/PlaceStilo.java
@@ -1,12 +1,15 @@
 package fribot2009;
 
+import lejos.nxt.ColorSensor;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.Navigator;
 
 public class PlaceStilo extends BehaviorParent {
 
-	public PlaceStilo(DifferentialPilot pilot, Navigator navi) {
-		super(pilot, navi, null, null, null, null);
+	public PlaceStilo(DifferentialPilot pilot, Navigator navi, UltrasonicSensor us, ColorSensor cs, TouchSensor ts1, TouchSensor ts2) {
+		super(pilot, navi, us,cs,ts1,ts2);
 		// TODO Auto-generated constructor stub
 	}
 

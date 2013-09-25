cec4e0284936a7c12abae0a3e3e717d78bc851d6
Cyrill <Cyrill@ReAlSwIsS>
2013-09-25 13:09:21 +0200

    .

--- PIDBot/src/pidBot/BehaviorParent.java
+++ PIDBot/src/pidBot/BehaviorParent.java
@@ -1,5 +1,41 @@
 package pidBot;
 
-public class BehaviorParent {
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.Sound;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.subsumption.Behavior;
 
+public abstract class BehaviorParent implements Behavior {
+
+	private boolean supressed=false;
+	protected DifferentialPilot pilot=Constants.PILOT;
+	protected Navigator navi=Constants.NAVI;
+	protected TouchSensor ts1=Constants.TS1;
+	protected TouchSensor ts2=Constants.TS2;
+	protected UltrasonicSensor us=Constants.US;
+	protected LightSensor light=Constants.LIGHT;
+	protected OdometryPoseProvider pose=Constants.POSE;
+	
+	@Override
+	public void action() {
+		supressed=false;
+		LCD.clear(2);
+		LCD.drawString(this.toString(), 0, 2);
+		Sound.playTone(2500, 500);
+	}
+
+	@Override
+	public void suppress() {
+		supressed=true;
+		
+	}
+
+
+	
+	
 }
---
+++ PIDBot/src/pidBot/Calibrate.java
@@ -0,0 +1,5 @@
+package pidBot;
+
+public class Calibrate {
+
+}

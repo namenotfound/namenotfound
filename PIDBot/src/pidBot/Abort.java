5a74f5582c8edfbbc3ea7a92f4b2f453b6ba5631
namenotfound <_>
2013-09-25 13:13:27 +0200

    .

--- PIDBot/src/pidBot/Abort.java
+++ PIDBot/src/pidBot/Abort.java
@@ -1,5 +1,16 @@
 package pidBot;
 
-public class Abort {
+import lejos.nxt.Button;
 
+public class Abort extends BehaviorParent {
+
+	public boolean takeControl(){
+		return Button.ESCAPE.isDown();
+	}
+	
+	public void action(){
+		super.action();
+		System.exit(0);
+	}
+	
 }

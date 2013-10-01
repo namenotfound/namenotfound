299090c9779c8e004fb28166ac5c5af3503909a8
Cyrill <Cyrill@ReAlSwIsS>
2013-10-01 11:13:31 +0200

    cfsfds

---
+++ HSLUChallange/src/hsluchallange/HSLUChallange.java
@@ -0,0 +1,32 @@
+package hsluchallange;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+import lejos.util.Delay;
+
+public class HSLUChallange {
+
+	
+	public static void main(String[] args)
+	{
+		LCD.drawString("PID Bot", 0, 0);
+		Button.waitForAnyPress();
+		Delay.msDelay(1000);
+		Constants.init();
+		Behavior[] behavior={
+				//new Calibrate(),
+				new Abort()
+		};
+		
+		
+		Arbitrator arbi=new Arbitrator(behavior);
+		
+		arbi.start();
+		
+	}
+	
+	
+	
+}

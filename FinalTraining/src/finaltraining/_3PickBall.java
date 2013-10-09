54d27f751028c43481bf5d31e56f1a63261b82df
namenotfound <_>
2013-10-09 14:16:36 +0200

    .

---
+++ FinalTraining/src/finaltraining/_3PickBall.java
@@ -0,0 +1,50 @@
+package finaltraining;
+
+import lejos.robotics.Color;
+import lejos.util.Delay;
+
+public class _3PickBall extends BehaviorParent {
+	
+	private int detectedColor;
+	
+	@Override
+	public String toString() {
+		return "Pick Ball";
+	}
+	
+	@Override
+	public void action() {
+		super.action();
+		
+		pilot.setAcceleration(Constants.DEFAULTACCELERATON);
+		pilot.setTravelSpeed(Constants.SPEEDFAST);
+		// rückwärts fahren
+		pilot.travel(-10);
+		if(!waitForStop()){
+			return;
+		}
+		// vors Blatt fahren
+		navi.goTo(250, 50, 0);
+		if(!waitForStop()){
+			return;
+		}
+		// suche farbe auf blatt
+		pilot.forward();
+		while (cs.getColor().getColor() != Color.BLUE && cs.getColor().getColor() != Color.RED) {
+			if(suppressed){
+				return;
+			}
+			Delay.msDelay(50);
+		}
+		pilot.stop();
+		detectedColor = cs.getColor().getColor();
+		
+		executed = true;
+	}
+	
+	public int getDetectedColor(){
+		return detectedColor;
+	}
+	
+
+}
